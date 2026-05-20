225c9ddbb6f9f61c88b53f0c0ca1a527d4dbd066
quuee <bandzuangon@gmail.com>
2026-05-20 21:01:06 +0800

    up

--- app/src/main/java/cn/x/dailycost/ui/screen/hold/HoldScreen.kt
+++ app/src/main/java/cn/x/dailycost/ui/screen/hold/HoldScreen.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -18,6 +20,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.RemoveRedEye
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -121,13 +124,22 @@ fun HoldScreen(
                     )
                 })
         }
-        items(state.goodsItems) { goods ->
+        if (state.isLoading) {
+            item {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    CircularProgressIndicator() // 加载中转圈圈
+                }
+            }
+        } else {
+            items(state.goodsItems) { goods ->
 
-            GoodsItem(
-                goods,
-                bgColor = state.categoryMap[goods.cid]?.color ?: Color(0XFFF8F1E4).toArgb(),
-                onToggle = { navController.navigate(Routes.Goods.route + "/${it}") })
+                GoodsItem(
+                    goods,
+                    bgColor = state.categoryMap[goods.cid]?.color ?: Color(0XFFF8F1E4).toArgb(),
+                    onToggle = { navController.navigate(Routes.Goods.route + "/${it}") })
+            }
         }
+
     }
 }
 
